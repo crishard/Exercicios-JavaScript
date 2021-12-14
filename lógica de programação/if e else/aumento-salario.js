ddca5822f05d86f34536a41a6aaae2aacf3511ee
crishard <crislantorres9@gmail.com>
2021-12-14 16:24:31 -0300

    correção

--- lógica de programação/if e else/aumento-salario.js
+++ lógica de programação/if e else/aumento-salario.js
@@ -1,9 +1,7 @@
 /*As Organizações Tabajara resolveram dar um aumento de salário aos seus colaboradores e lhe 
 contrataram para desenvolver um script que calculará os reajustes.
-
 Faça um script que recebe o salário de um colaborador e o reajuste segundo o seguinte critério, 
 baseado no salário atual:
-
     salários até R$ 280,00 (incluindo) : aumento de 20%
     salários entre R$ 280,00 e R$ 700,00 : aumento de 15%
     salários entre R$ 700,00 e R$ 1500,00 : aumento de 10%
--- lógica de programação/if e else/triangulo.js
+++ lógica de programação/if e else/triangulo.js
@@ -1,7 +1,6 @@
 /**Faça um script que peça os 3 lados de um triângulo. O script deverá informar se os valores podem 
  * ser um triângulo. Indique, caso os lados formem um triângulo, se o mesmo é: equilátero, isósceles 
  * ou escaleno.
-
     Dicas:
     Três lados formam um triângulo quando a soma de quaisquer dois lados for maior que o terceiro;
     Triângulo Equilátero: três lados iguais;
@@ -28,4 +27,3 @@ if(lado1 + lado2 > lado2 || lado2 + lado3 > lado1 || lado1 + lado3 > lado2){
 else{
     console.log('Os valores não formam um triângulo')
 }
-
