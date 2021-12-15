2352b2e89c6a8e597196192577a3ab100abacaed
crishard <crislantorres9@gmail.com>
2021-12-15 10:00:46 -0300

    new

---
+++ lógica de programação/date/dia-mes.js
@@ -0,0 +1,88 @@
+const data = new Date();
+
+
+function getDiaTexto(diaSemana){
+    let diaSemanaTexto;
+    switch(diaSemana){
+        case 0:
+            diaSemanaTexto = 'Domingo';
+            return diaSemanaTexto;
+        case 1:
+            diaSemanaTexto = 'Segunda';
+            return diaSemanaTexto;
+        case 2:
+            diaSemanaTexto = 'Terça';
+            return diaSemanaTexto;
+        case 3:
+            diaSemanaTexto = 'Quarta';
+            return diaSemanaTexto;
+        case 4:
+            diaSemanaTexto = 'Quinta';
+            return diaSemanaTexto;
+        case 5:
+            diaSemanaTexto = 'Sexta';
+            return diaSemanaTexto;
+        case 6:
+            diaSemanaTexto = 'Sábado';
+            return diaSemanaTexto;
+        default:
+            diaSemanaTexto = '';
+            return diaSemanaTexto;
+    }
+}
+function getMesTexto(numeroMes){
+    let mesTexto;
+    switch(numeroMes){
+        case 0:
+            mesTexto = 'Janeiro';
+            return mesTexto;
+        case 1:
+            mesTexto = 'Fevereiro';
+            return mesTexto;
+        case 2:
+            mesTexto = 'Março';
+            return mesTexto;
+        case 3:
+            mesTexto = 'Abril';
+            return mesTexto;
+        case 4:
+            mesTexto = 'Maio';
+            return mesTexto;
+        case 5:
+            mesTexto = 'Junho';
+            return mesTexto;
+        case 6:
+            mesTexto = 'Julho';
+            return mesTexto;
+        case 7:
+            mesTexto = 'Agosto';
+            return mesTexto;
+        case 8:
+            mesTexto = 'Setembro';
+            return mesTexto;
+        case 9:
+            mesTexto = 'Outubro';
+            return mesTexto;
+        case 10:
+            mesTexto = 'Novembro';
+            return mesTexto;
+        case 11:
+            mesTexto = 'Dezembro';
+            return mesTexto;
+        default:
+            mesTexto = '';
+            return mesTexto
+    }
+}
+
+function criaData(data){
+    const diaSemana = data.getDay();
+    const numeroMes = data.getMonth();
+
+    const nomeDia = getDiaTexto(diaSemana);
+    const nomeMes = getMesTexto(numeroMes);
+
+    return `${nomeDia}, ${data.getDate()} de ${nomeMes} de ${data.getFullYear()}`
+}
+
+console.log(criaData(data));
