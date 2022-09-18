1af449983bcc9675e66581f575c4006a12cdf0d1
crishard <crislantorres9@gmail.com>
2022-09-18 11:01:34 -0300

    polimorfismo com class

--- lógica de programação/objetos/polimorfismo/polimorfismo.js
+++ lógica de programação/objetos/polimorfismo/polimorfismo.js
@@ -1,27 +1,28 @@
 // Banco 
-function Conta(agencia, conta, saldo){
-    this.conta = conta;
-    this.saldo = saldo;
-    this.agencia = agencia;
+class Conta {
+    constructor(agencia, conta, saldo) {
+        this.conta = conta;
+        this.saldo = saldo;
+        this.agencia = agencia;
+    }
+    deposito(valor) {
+        this.saldo += valor;
+        this.verSaldo();
+    }
+    sacar(valor) {
+        if (valor > this.saldo)
+            return "Saldo insuficiente para saque";
+
+        this.saldo -= valor;
+        this.verSaldo();
+    }
+    verSaldo() {
+        console.log('Ag:', this.agencia,
+            'Conta: ', this.conta,
+            'Saldo: ', this.saldo);
+    }
 };
 
-
-Conta.prototype.deposito = function(valor) {
-    this.saldo += valor;
-    this.verSaldo();
-}
-
-Conta.prototype.sacar = function(valor) {
-    if(valor > this.saldo) return "Saldo insuficiente para saque";
-
-    this.saldo -= valor
-    this.verSaldo();
-};
-Conta.prototype.verSaldo = function(){
-    console.log('Ag:', this.agencia,
-    'Conta: ', this.conta,
-    'Saldo: ', this.saldo);
-}
 
 const conta01 = new Conta(12, 94739493, 10);
 
