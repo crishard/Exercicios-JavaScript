0b86a9829b0997b6acc236a6b8b1099de9e5b6ec
crishard <crislantorres9@gmail.com>
2022-09-18 10:59:58 -0300

    polimorfismo com prototype

--- lógica de programação/objetos/polimorfismo/polimorfismo.js
+++ lógica de programação/objetos/polimorfismo/polimorfismo.js
@@ -4,3 +4,25 @@ function Conta(agencia, conta, saldo){
     this.saldo = saldo;
     this.agencia = agencia;
 };
+
+
+Conta.prototype.deposito = function(valor) {
+    this.saldo += valor;
+    this.verSaldo();
+}
+
+Conta.prototype.sacar = function(valor) {
+    if(valor > this.saldo) return "Saldo insuficiente para saque";
+
+    this.saldo -= valor
+    this.verSaldo();
+};
+Conta.prototype.verSaldo = function(){
+    console.log('Ag:', this.agencia,
+    'Conta: ', this.conta,
+    'Saldo: ', this.saldo);
+}
+
+const conta01 = new Conta(12, 94739493, 10);
+
+conta01.deposito(10);
